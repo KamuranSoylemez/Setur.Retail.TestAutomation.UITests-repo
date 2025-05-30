78d89da111998b719834fdb96acd30458e7bae2d
Kamuran Söylemez <kamuran.soylemez@setur.com.tr>
2025-05-30 14:18:54 +0300

    failed login cases

--- src/test/java/pages/commonPages/LoginPage.java
+++ src/test/java/pages/commonPages/LoginPage.java
@@ -1,6 +1,7 @@
 package pages.commonPages;
 
 import com.microsoft.playwright.Locator;
+import org.junit.Assert;
 import utils.UserDataReader;
 
 public class LoginPage extends BasePage {
@@ -20,4 +21,26 @@ public class LoginPage extends BasePage {
         clickElement(loginButton);
     }
 
+    public void tryLoginWithUseAndPass(String user, String pass) {
+        userNameLocator.fill(user);
+        passwordLocator.fill(pass);
+    }
+    public void verifyUnsuccessfulLogin() {
+        Locator warningMessage = page.locator(".ajs-message.ajs-error.ajs-visible");
+
+        if (!warningMessage.isVisible()){
+            // input-validation-error sınıfı kontrolü
+            String usernameClass = userNameLocator.getAttribute("class");
+            String passwordClass = passwordLocator.getAttribute("class");
+
+            Assert.assertTrue("Kullanıcı adı alanı doğrulama hatası içermeli",
+                    usernameClass.contains("input-validation-error"));
+            Assert.assertTrue("Şifre alanı doğrulama hatası içermeli",
+                    passwordClass.contains("input-validation-error"));
+            }
+        else {
+            Assert.assertTrue(warningMessage.isVisible());
+        }
+    }
 }
+
--- src/test/resources/login.feature
+++ src/test/resources/login.feature
@@ -8,3 +8,14 @@ Feature: Login Page
       And click login
       Then verify successful login
 
+  Scenario Outline: Unsuccessful Login
+    When try login with "<username>" and "<password>"
+    And click login
+    Then verify unsuccessful login
+
+    Examples: User and Pass
+      | username | password |
+      | user     | pass     |
+      |          |          |
+      | admin    | 1234     |
+
